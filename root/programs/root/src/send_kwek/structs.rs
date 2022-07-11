2fe25316ce5327414e125c03d0e6c4703bc313cf
maxefi <maxefi@mail.ru>
2022-07-11 23:53:59 +0700

    fix tweet typos

--- root/programs/root/src/send_kwek/structs.rs
+++ root/programs/root/src/send_kwek/structs.rs
@@ -1,12 +1,12 @@
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::system_program;
 
-use crate::tweet::structs::*;
+use crate::kwek::structs::*;
 
 #[derive(Accounts)]
 pub struct SendKwek<'info> {
     #[account(init, payer = author, space = Kwek::LEN)]
-    pub tweet: Account<'info, Tweet>,
+    pub kwek: Account<'info, Kwek>,
     #[account(mut)]
     pub author: Signer<'info>,
     pub system_program: Program<'info, System>,
