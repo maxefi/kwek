832c355e6db46342e7e238922b9e6ef02c5d721a
maxefi <maxefi@mail.ru>
2022-07-11 23:51:48 +0700

    rename tweet to kwek

--- root/programs/root/src/kwek/structs.rs
+++ root/programs/root/src/kwek/structs.rs
@@ -1,8 +1,8 @@
 use anchor_lang::prelude::*;
 
-// 1. Define the structure of the Tweet account.
+// 1. Define the structure of the Kwek account.
 #[account]
-pub struct Tweet {
+pub struct Kwek {
     pub author: Pubkey,
     pub timestamp: i64,
     pub topic: String,
@@ -17,8 +17,8 @@ const STRING_LENGTH_PREFIX: usize = 4; // Stores the size of the string.
 const MAX_TOPIC_LENGTH: usize = 50 * 4; // 50 chars max.
 const MAX_CONTENT_LENGTH: usize = 280 * 4; // 280 chars max.
 
-// 3. Add a constant on the Tweet account that provides its total size.
-impl Tweet {
+// 3. Add a constant on the Kwek account that provides its total size.
+impl Kwek {
     const LEN: usize = DISCRIMINATOR_LENGTH
       + PUBLIC_KEY_LENGTH // Author.
       + TIMESTAMP_LENGTH // Timestamp.
