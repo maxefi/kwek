02c0dcb02d8c72190a59c89ef9a8e20465bae551
maxefi <maxefi@mail.ru>
2022-07-14 11:01:33 +0700

    add app fetchTweets filters

--- root/app/src/api/fetch-tweets.js
+++ root/app/src/api/fetch-tweets.js
@@ -1,10 +1,31 @@
 import { useWorkspace } from "@/composables";
 import { Tweet } from "@/models";
+import bs58 from "bs58";
 
-export const fetchTweets = async () => {
+export const fetchTweets = async (filters = []) => {
   const { program } = useWorkspace();
 
-  const tweets = await program.value.account.kwek.all();
+  const tweets = await program.value.account.tweet.all(filters);
 
   return tweets.map((tweet) => new Tweet(tweet.publicKey, tweet.account));
 };
+
+export const authorFilter = (authorBase58PublicKey) => ({
+  memcmp: {
+    /*
+     * see `DISCRIMINATOR_LENGTH`
+     * */
+    offset: 8,
+    bytes: authorBase58PublicKey,
+  },
+});
+
+export const topicFilter = (topic) => ({
+  memcmp: {
+    /*
+     * `DISCRIMINATOR_LENGTH` + `PUBLIC_KEY_LENGTH` + `TIMESTAMP_LENGTH` + `STRING_LENGTH_PREFIX`
+     * */
+    offset: 8 + 32 + 8 + 4,
+    bytes: bs58.encode(Buffer.from(topic)),
+  },
+});
